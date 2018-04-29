5dd1eee1e35bc0fc02378e55c3b06be66b9bb0e5
suomiy <suomiy@gmail.com>
2018-04-30 01:05:37 +0200

    fix postgres

--- benchmarking/benchmarks/postgres/standard-run.sh
+++ benchmarking/benchmarks/postgres/standard-run.sh
@@ -4,5 +4,8 @@ set -eu
 
 CLIENTS="`echo "$THREADS * 2" | bc`" # clients running simultaneously
 
+# wait for start
+systemctl start postgresql-10 || exit -1
+
 # run benchmark
 sudo -i -u postgres /usr/pgsql-10/bin/pgbench --jobs="$THREADS" --client="$CLIENTS"  "$NAME" --time="$RUN_TIME"
