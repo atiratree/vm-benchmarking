47c8187c218597ded8ec49609d6fe0894c932fb5
suomiy <suomiy@gmail.com>
2018-09-10 23:32:51 +0200

    add init-git.sh hook option for initializing versioning
    - must be put inside bin/generated folder

--- benchmarking/bin/util/version-results.sh
+++ benchmarking/bin/util/version-results.sh
@@ -3,11 +3,14 @@
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BENCHMARKS_DIR="`realpath "$SCRIPTS_DIR/../../benchmarks/"`"
+GENERATED_DIR="`realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../generated"`"
 
 BENCH_GIT_RESULTS="${BENCH_GIT_RESULTS:-/tmp/benchmark-run-results}"
 BENCH_EDITABLE_CFG="$BENCH_GIT_RESULTS/benchmark-suite.editable-cfg"
 BENCH_OUTPUT="$BENCH_GIT_RESULTS/benchmark-suite.output"
 COMMIT_TIME="$BENCH_GIT_RESULTS/time"
+
+INIT_GIT_SCRIPT="$GENERATED_DIR/init-git.sh"
 
 MESSAGE="$1"
 FORCE_REMOVE="${FORCE_REMOVE:-}"
@@ -17,10 +20,20 @@ if [ -z "$MESSAGE" ]; then
     exit 1
 fi
 
-if [ ! -d "$BENCH_GIT_RESULTS/.git" ]; then
-    echo "$BENCH_GIT_RESULTS must be initialized git directory" >&2
-    exit 2
+pushd tmp
+if [ ! -d "$BENCH_GIT_RESULTS/.git" ] || ! git status &> /dev/null; then
+    if [ -x "$INIT_GIT_SCRIPT" ]; then
+        "$INIT_GIT_SCRIPT"
+        if [ ! -d "$BENCH_GIT_RESULTS/.git" ] || ! git status &> /dev/null; then
+            echo "$INIT_GIT_SCRIPT failed to produce valid $BENCH_GIT_RESULTS" >&2
+            exit 2
+        fi
+    else
+        echo "$BENCH_GIT_RESULTS must be initialized git directory (hint: use init-git.sh in /tmp for that)" >&2
+        exit 2
+    fi
 fi
+popd
 
 copy_first_file_starts(){
     for file in "$1"*; do
